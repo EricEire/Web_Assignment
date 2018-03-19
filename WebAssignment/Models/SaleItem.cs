49c008da011007a95f1b9968f4a4911910b85b76
Tassadar22 <seamas2@gmail.com>
2018-03-19 20:50:57 +0000

    Added Saleitem model

---
+++ WebAssignment/Models/SaleItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAssignment.Models
+{
+    public class SaleItem
+    { //Model page with generic item which is based off HTML code for each page
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ItemImage { get; set; }
+        public string ItemType { get; set; }
+
+        public SaleItem() { }
+        public SaleItem(string title, decimal price, string description, string itemimage, string itemtype)
+        {
+            Title = title;
+            Price = price;
+            Description = description;
+            ItemImage = itemimage;
+            ItemType = itemtype;
+        }
+    }
+}
