f0ed21abda212202e28723f0ce3a359c2522b486
Tassadar22 <seamas2@gmail.com>
2018-04-05 13:27:45 +0100

    added usermodel for logging in

---
+++ WebAssignment/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAssignment.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [RegularExpression("[A-Za-z]+")]
+        [Display(Name ="User Name")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Password must be 5 to 20 characters long", MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        //Consider putting userrole in here
+
+    }
+}
