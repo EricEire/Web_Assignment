1e9fe88a73c0ab53ff588ccfcec45ca6d30e22be
Eric Scott <Eric-_-Scott@hotmail.com>
2018-04-07 15:16:38 +0100

    PUSH es

---
+++ SQLQueryCreateProductTable.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Product (
+    ProductID          VARCHAR (20)    NOT NULL PRIMARY KEY,
+    ProductType         VARCHAR (20),
+    ProductName         VARCHAR (50),
+    ProductDescription VARCHAR (100),
+    ProductPricePerUnit DECIMAL (5, 2),
+    ProductQuantity    INT            ,
+    ProductSize         VARCHAR (10)   ,
+    ProductColour       VARCHAR (50)    ,
+    ProductImage        VARBINARY (MAX) ,
+    
+)
+
