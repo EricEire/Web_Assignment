04d32044a858deca7aea7cc8049bc243aec9c98e
Eric Scott <Eric-_-Scott@hotmail.com>
2018-04-07 14:17:13 +0100

    Products added to database

---
+++ AddingShirts.sql
@@ -0,0 +1,5 @@
+﻿
+DECLARE @img AS VARBINARY(MAX)
+SET @img = (SELECT BulkColumn FROM OPENROWSET(BULK'C:\code\WebAssignment\Images\Shirts\Shirt_Black_1.jpg', SINGLE_BLOB)AS MyFile)
+INSERT INTO Product(ProductID,ProductType, ProductName, ProductDescription, ProductPricePerUnit, ProductQuantity,ProductSize,ProductColour,ProductImage)
+VALUES('SH101','Shirt','Black Shirt','An equisite black shirt',20.00,5,'Medium','Black',@img);
