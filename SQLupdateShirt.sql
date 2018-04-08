d07a6a2b80db92854edcf625b80d7cf5cf0dd449
Eric Scott <Eric-_-Scott@hotmail.com>
2018-04-08 14:48:58 +0100

    CSS fix and Shirt Image fix

---
+++ SQLupdateShirt.sql
@@ -0,0 +1,6 @@
+﻿Go
+DECLARE @img AS VARBINARY(MAX)
+SET @img = (SELECT BulkColumn FROM OPENROWSET(BULK'C:\code\WebAssignment\Images\Shirts\Shirt_Blue_2.jpg', SINGLE_BLOB)AS MyFile)
+UPDATE Product
+SET ProductImage = @Img
+where ProductID='SH104'
